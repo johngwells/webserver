c1af50b0476d5f1c54440714cbe68cfed75ecb17
John Wells <molekewl2017@gmail.com>
2021-12-31 00:24:58 -0800

    using node streams, http request & response

--- index.js
+++ index.js
@@ -11,34 +11,43 @@ const friends = [
     id: 1,
     name: 'Trinity'
   }
-]
+];
+
+// test in developer tools
+// fetch('http://localhost:3000/friends', { method: 'POST', body: JSON.stringify({ id: 3, name: 'Ryan Dahl' })}).then(response => response.json()).then(friend => console.log(friend))
 
 const server = http.createServer((req, res) => {
-  const items = req.url.split('/')
-  if (items[1] === 'friends') {
+  const items = req.url.split('/');
+  if (req.method === 'POST' && items[1] === 'friends') {
+    req.on('data', data => {
+      const friend = data.toString();
+      console.log('Request:', data); // convert to string to print it
+      friends.push(JSON.parse(friend)); // convert to object & pass to the array
+    });
+    req.pipe(res);
+  } else if (req.method === 'GET' && items[1] === 'friends') {
     res.writeHead(200, {
       'Content-Type': 'application/json'
     });
     if (items.length === 3) {
-      const friendIndex = Number(items[2])
-      res.end(JSON.stringify(friends[friendIndex]))
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
     }
-    res.end(
-      JSON.stringify(friends)
-    );
-  } else if (items[1] === 'messages') {
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<body>')
-    res.write('<ul>')
-    res.write('<li>Hello</li>')
-    res.write('<ul>')
-    res.write('<body>')
-    res.write('<html>')
-    res.end()
+  } else if (req.method === 'GET' && items[1] === 'messages') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li>Hello</li>');
+    res.write('</ul>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
   } else {
     res.statusCode = 404;
-    res.end()
+    res.end();
   }
 });
 
